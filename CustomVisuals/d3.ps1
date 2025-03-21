6ab36e05342ae472ba8fc5d5171aaece5becf56e
Raleigh West <44733222+westrale@users.noreply.github.com>
2025-03-21 00:48:12 -0500

    Add files via upload

---
+++ CustomVisuals/d3.ps1
@@ -0,0 +1,16 @@
+﻿npm i d3@3.5.5
+
+#####################################################################
+# + d3@3.5.5
+# added 1 package from 2 contributors and audited 1 package in 1.794s
+# found 0 vulnerabilities
+#####################################################################
+
+npm i @types/d3@3.5
+
+####################################################################
+# + @types/d3@3.5.42
+# added 1 package from 4 contributors and audited 2 packages in 1.113s
+# found 0 vulnerabilities
+####################################################################
+
---
+++ CustomVisuals/pbiviz_install_cert.ps1
@@ -0,0 +1,14 @@
+﻿pbiviz --install-cert
+
+####################################################################################
+# PS C:\Users\westr> pbiviz --install-cert
+# pbiviz :  error  Certificate not found. The new certificate will be generated
+# At line:1 char:1
+# + pbiviz --install-cert
+# + ~~~~~~~~~~~~~~~~~~~~~
+#     + CategoryInfo          : NotSpecified: ( error  Certifi...ll be generated:String) [], RemoteException
+#     + FullyQualifiedErrorId : NativeCommandError
+#  
+#  info   Certificate generated. Location is C:\Users\westr\AppData\Roaming\npm\node_modules\powerbi-visuals-tools\certs\PowerBICustomVi
+# sualTest_public.pfx. Passphrase is '5939709721264657'
+###################################################################################
